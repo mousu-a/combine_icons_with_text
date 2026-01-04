fb1a1b2fd1e6ee96800f646a6291db8ebda35ea8
appsu-mi <natubate870@gmail.com>
2026-01-04 12:01:06 +0900

    モデルでもバリデーションを追加 user

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   has_many :original_icons, dependent: :destroy
 
   validates :uid, uniqueness: { scope: :provider }
+  validates :uid, :provider, presence: true
 end
