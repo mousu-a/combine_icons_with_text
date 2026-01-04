267cf6fc63c1eef13dae2724da5d6d53972edc41
appsu-mi <natubate870@gmail.com>
2026-01-04 12:16:25 +0900

    マイグレーションファイルを修正　ロールバックできるように

--- db/migrate/20260104025511_remove_unique_index_from_icon_change_links.rb
+++ db/migrate/20260104025511_remove_unique_index_from_icon_change_links.rb
@@ -1,5 +1,8 @@
 class RemoveUniqueIndexFromIconChangeLinks < ActiveRecord::Migration[8.0]
   def change
-    remove_index :icon_change_links, name: "index_icon_change_links_on_site_name_and_url"
+    remove_index :icon_change_links,
+      [:site_name, :url],
+      unique: true,
+      name: "index_icon_change_links_on_site_name_and_url"
   end
 end
