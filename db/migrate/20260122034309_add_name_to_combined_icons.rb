ae3b1cff26e55118f4b45a9d56f7582d4689fdf3
appsu-mi <natubate870@gmail.com>
2026-04-26 08:32:31 +0900

    マイグレーション修正　既存データがある環境では失敗する可能性があるため まだ本番環境にデータは入っていないため問題はないが、勉強のためやっておく

--- db/migrate/20260122034309_add_name_to_combined_icons.rb
+++ db/migrate/20260122034309_add_name_to_combined_icons.rb
@@ -1,5 +1,17 @@
 class AddNameToCombinedIcons < ActiveRecord::Migration[8.0]
-  def change
-    add_column :combined_icons, :name, :string, null: false
+  def up
+    add_column :combined_icons, :name, :string
+
+    execute <<~SQL.squish
+      UPDATE combined_icons
+      SET name = '名称未設定'
+      WHERE name IS NULL
+    SQL
+
+    change_column_null :combined_icons, :name, false
+  end
+
+  def down
+    remove_column :combined_icons, :name
   end
 end
