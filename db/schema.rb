42bd8194ca516c7137aa0f9d4b82c26906e5cf77
appsu-mi <natubate870@gmail.com>
2026-05-12 09:41:27 +0900

    usersにadminフラグを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2026_01_22_034309) do
+ActiveRecord::Schema[8.1].define(version: 2026_05_12_003039) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -225,6 +225,7 @@ ActiveRecord::Schema[8.0].define(version: 2026_01_22_034309) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.boolean "admin", default: false, null: false
     t.string "name", null: false
     t.string "provider", null: false
     t.string "uid", null: false
