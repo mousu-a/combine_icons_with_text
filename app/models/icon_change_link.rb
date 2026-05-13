5023a4eb1e607532736ea14c296aceb2990f85f3
appsu-mi <natubate870@gmail.com>
2026-05-13 12:46:40 +0900

    IconChangeLink:urlのバリデーションを追加 httpであることを保証させる

--- app/models/icon_change_link.rb
+++ app/models/icon_change_link.rb
@@ -3,4 +3,5 @@
 class IconChangeLink < ApplicationRecord
   validates :site_name, presence: true, uniqueness: true
   validates :url, presence: true, uniqueness: true
+  validates :url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]) }
 end
