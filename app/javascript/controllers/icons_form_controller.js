f1b375dd51be624fc0a76b29eb27c4076e4d7579
appsu-mi <natubate870@gmail.com>
2026-05-16 13:00:45 +0900

    使っていないtargetを削除

--- app/javascript/controllers/icons_form_controller.js
+++ app/javascript/controllers/icons_form_controller.js
@@ -1,7 +1,7 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["uploadedIcon", "originalImage", "toast", "toastMessage"];
+  static targets = ["toast", "toastMessage"];
 
   async submit(event) {
     const fd = setupFormdata(event.detail);
