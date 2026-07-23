c103de54ff249f9122a4d8bce01e130fbf98b234
appsu-mi <natubate870@gmail.com>
2026-07-23 13:14:43 +0900

    アイコンが選択された後、アップロードボタンの表示やテキストを目立たなくする

---
+++ app/javascript/controllers/ui_state_controller.js
@@ -0,0 +1,13 @@
+import { Controller } from "@hotwired/stimulus";
+
+export default class extends Controller {
+  static targets = ["instructionText", "uploadButton"];
+
+  change() {
+    this.instructionTextTarget.textContent = "アイコンを変更したい場合はこちら";
+    this.uploadButtonTarget.textContent = "アイコンを変更";
+
+    this.uploadButtonTarget.classList.add("is-muted");
+    this.instructionTextTarget.classList.add("is-muted");
+  }
+}
