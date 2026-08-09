778456e641a15e9cd327b9140e83f2f95a4f7f87
appsu-mi <natubate870@gmail.com>
2026-08-09 10:55:48 +0900

    userのリクエストスペックを追加

---
+++ spec/requests/user_spec.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe 'User' do
+  describe 'DELETE /logout' do
+    let(:user) { create(:user) }
+
+    before { login(user) }
+
+    it 'logs out' do
+      delete logout_path
+
+      expect(response).to redirect_to(root_path)
+      follow_redirect!
+      expect(response.body).to include('ログアウトしました')
+    end
+  end
+
+  describe 'DELETE /users/:id' do
+    let(:user) { create(:user) }
+
+    before { login(user) }
+
+    it 'deletes a user account' do
+      expect do
+        delete user_path(user)
+      end.to change(User, :count).by(-1)
+
+      expect(session[:user_id]).to be_nil
+      expect(response).to redirect_to(root_path)
+      follow_redirect!
+      expect(response.body).to include('退会しました')
+    end
+  end
+end
