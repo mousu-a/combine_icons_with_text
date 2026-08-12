87fb418f8c6166dfe675059a84bdd360a778b144
machida <machida@fjord.jp>
2026-08-13 03:05:07 +0900

    画像アップロードをドラッグ中心の構成へ変更

--- app/javascript/controllers/ui_state_controller.js
+++ app/javascript/controllers/ui_state_controller.js
@@ -1,10 +1,9 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["instructionText", "uploadButton", "editControls"];
+  static targets = ["uploadButton", "editControls"];
 
   change() {
-    this.instructionTextTarget.textContent = "画像を選択しました";
     this.uploadButtonTarget.textContent = "画像を変更";
     this.uploadButtonTarget.classList.add("is-secondary");
     this.editControlsTarget.disabled = false;
