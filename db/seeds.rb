3289f5d25f2854920104efe5a6e903ab82aa3782
appsu-mi <natubate870@gmail.com>
2026-08-17 10:14:19 +0900

    テスト環境でもseedを実行しないように（テストでの必要な情報はファクトリで作る）

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,8 @@
 # frozen_string_literal: true
 
+unless Rails.env.development?
+  Rails.logger.info('Skip loading seed data outside the development environment.')
+  return
+end
+
 load Rails.root.join('db/seeds/icon_change_links.rb')
