605621eeb1ca6a24e94478899b90975a29eecdab
appsu-mi <natubate870@gmail.com>
2026-08-20 10:24:48 +0900

    汎用的にリネーム

--- app/javascript/controllers/dropdown_controller.js
+++ app/javascript/controllers/dropdown_controller.js
@@ -1,9 +1,9 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["logoutButton"];
+  static targets = ["accountMenu"];
 
-  toggleLogoutButton() {
-    this.logoutButtonTarget.classList.toggle("hidden");
+  toggle() {
+    this.accountMenuTarget.classList.toggle("hidden");
   }
 }
