bb026eb981e970a2ecccbc16231f4c7722681360
appsu-mi <natubate870@gmail.com>
2026-08-26 09:45:59 +0900

    ドラッグ形メソッドをui_stateコントローラに移譲

--- app/javascript/controllers/ui_state_controller.js
+++ app/javascript/controllers/ui_state_controller.js
@@ -1,11 +1,27 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["uploadButton", "editControls"];
+  static targets = ["uploadButton", "editControls", "dropZone"];
 
   change() {
     this.uploadButtonTarget.textContent = "画像を変更";
     this.uploadButtonTarget.classList.add("is-secondary");
     this.editControlsTarget.disabled = false;
+  }
+
+  dragOver(e) {
+    e.preventDefault();
+    e.dataTransfer.dropEffect = "copy";
+  }
+
+  dragEnter(e) {
+    e.preventDefault();
+    this.dropZoneTarget.classList.add("is-dragging");
+  }
+
+  dragLeave(e) {
+    if (e.relatedTarget && e.currentTarget.contains(e.relatedTarget)) return;
+
+    this.dropZoneTarget.classList.remove("is-dragging");
   }
 }
