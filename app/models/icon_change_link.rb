29586cb7cb1e61e8d4a34da31b1603671e9c02c8
appsu-mi <natubate870@gmail.com>
2026-08-29 13:23:53 +0900

    バリデーションの記述を整理

--- app/models/icon_change_link.rb
+++ app/models/icon_change_link.rb
@@ -1,8 +1,7 @@
 # frozen_string_literal: true
 
 class IconChangeLink < ApplicationRecord
-  validates :site_name, presence: true, uniqueness: true
-  validates :url, presence: true, uniqueness: true
-  validates :url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]) }
+  validates :site_name, :url, presence: true, uniqueness: true
   validates :guide_text, presence: true
+  validates :url, format: { with: URI::DEFAULT_PARSER.make_regexp(%w[http https]) }
 end
